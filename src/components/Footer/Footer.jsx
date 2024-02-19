51a9cdad3adf92ac7a1af57f34f329f754c5d566
Aziz <karimberdiyevaziz077@gmail.com>
2024-02-19 17:51:00 +0500

    footer-final

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Footer.scss'
 import { Link } from 'react-scroll'
+import Facebook from "../../assets/Facebook.svg"
+import Insta from "../../assets/Insta.svg"
+import Twitter from "../../assets/Twitter.svg"
 
 const Footer = () => {
   return (
@@ -87,6 +90,37 @@ const Footer = () => {
                     <Link className="footer__text">
                         Contact Us
                     </Link>
+                </div>
+                    <div className="footer__wrapper-icon">
+                        <Link className='footer__title2'>
+                            FOR GAMERS. BY GAMERS.™
+                        </Link>
+                        <div className="footer__flex-icon">
+                            <Link>
+                                <img width={30} src={Facebook} alt="asd" />
+                            </Link>
+                            <Link>
+                                <img width={30} src={Insta} alt="asd" />
+                            </Link>
+                            <Link>
+                                <img width={30} src={Twitter} alt="asd" />
+                            </Link>
+                        </div>
+                    </div>
+            </div>
+            <div className="footer__black">
+                <div className="footer__flex-link">
+                    <Link className='footer__title3'>
+                        Copyright © 2022 Razer Inc. All rights reserved.
+                    </Link>
+                    <div className="footer__link-text">
+                        <Link className='footer__text2-about'>
+                            Created by: karimberdiyev_aziz
+                        </Link>
+                        <Link className='footer__text2'>
+                            IT Academy
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
